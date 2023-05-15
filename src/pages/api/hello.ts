1329977d8d11a81cfba4f61c0ccc20412c1c5cf2
Nikhilesh Debbarma <nikhilesh.debbarma062@gmail.com>
2023-05-15 20:40:09 +0530

    mongodb database service

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { connectToDatabase } from '@/services/database/db';
 type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const { db } = await connectToDatabase();
   res.status(200).json({ name: 'John Doe' })
 }
---
+++ src/services/database/db.ts
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+
+
+
+
+let cachedClient: any = null;
+let cachedDb: any = null;
+
+export async function connectToDatabase() {
+
+    if (cachedClient && cachedDb) {
+        return {
+            client: cachedClient,
+            db: cachedDb,
+        };
+    }
+    let client = new MongoClient(process.env.MONGODB_URL);
+    await client.connect().then(() => console.log("Connected Successfully")).catch((err: any) => console.log(err))
+
+
+    let db = client.db('rentmeroom');
+    cachedClient = client;
+    cachedDb = db;
+    return {
+        client: cachedClient,
+        db: cachedDb,
+    }
+}
+
