51c4d5db3813727772ee51449d9cebca3a6aed48
Rudhamoy <rudhamoydebbarma@gmail.com>
2023-05-20 14:07:32 +0530

    mapview - list view added

--- app/components/homepage/RecentUpdates.tsx
+++ app/components/homepage/RecentUpdates.tsx
@@ -1,13 +1,23 @@
+"use client"
+import { useState } from 'react'
 import { data } from '@/assets/customData'
 import RoomCard from '../rooms/RoomCard'
 import styles from './home.module.css'
+import FilterButton from './FilterButton';
 
 const RecentUpdates = () => {
+    const [switchView, setSwitchView] = useState('listView')
+
     const length = 12;
     const myArray = Array.from({ length }, (_, index) => index + 1);
+
     return (
         <>
-            <div className={styles.recentUpdates__container}>
+            <FilterButton setSwitchView={setSwitchView} switchView={switchView} />
+            <div 
+            className={styles.recentUpdates__container}
+            style={{flexDirection: `${switchView !== 'listView' ? "column" : "row"}`}}
+            >
                 {myArray.splice(0, 4).map((i, index) => (
                     <RoomCard key={index} />
                 ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
+
 import FilterButton from "./components/homepage/FilterButton";
 import HeroSection from "./components/homepage/HeroSection";
 import RecentUpdates from "./components/homepage/RecentUpdates";
 
 
 export default function Home() {
+
   return (
     <div style={{marginBottom: "1rem"}}>
       <HeroSection />
-      <FilterButton />
       <RecentUpdates />
     </div>
   )
