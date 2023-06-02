348dee4b31e96bfd606b4643ec7b2c8c28152a6d
Rudhamoy <rudhamoydebbarma@gmail.com>
2023-06-02 21:00:05 +0530

    hello api

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import createUser from "@/services/models/user";
+import createUser from "../../../services/models/user";
 
 export async function GET(request: Request) {
     try {
