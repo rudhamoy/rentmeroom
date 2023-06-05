232dca84738f493691d5a096da5717e06dd7e095
Rudhamoy Debbarma <rudhamoydebbarma@gmail.com>
2023-06-05 21:18:48 +0530

    query db if owner exist with userId, else create

--- app/rooms/list/page.tsx
+++ app/rooms/list/page.tsx
@@ -7,9 +7,15 @@ import { redirect } from 'next/navigation';
 import { authOptions } from '@/api/auth/[...nextauth]/route';
 import getCurrentUser from '@/actions/getCurrentUser';
 import axios from 'axios'
+import CreateOwner from '@/components/owner/CreateOwner';
 
 const getRoomsData = async (user) => {
   const res = await axios.post('http://localhost:3000/api/rooms/owner', user)
+  return res.data
+}
+
+const checkExistOwner = async (id: string) => {
+  const res = await axios.post('http://localhost:3000/api/owner', {id})
   return res.data
 }
 
@@ -18,33 +24,46 @@ const RoomListPage = async () => {
   const currentUser = await getCurrentUser()
   const session = await getServerSession(authOptions)
 
-  if(!session?.user) {
+  let owner
+  let data
+  if (!session?.user) {
     redirect('/')
+  } else {
+    owner = await checkExistOwner(currentUser._id)
+    data = await getRoomsData(currentUser)
   }
-  
-  const data = await getRoomsData(currentUser)
+
   const numberOfRooms = data?.length || 0
 
+
   return (
-    <div className={styles.roomlist__container}>
-      <CreateRoom />
-      <div className={styles.roomlist__lists}>
-        <div>
-          <div>
-            {data ? data?.map((room) => (
-              <RoomCard key={room._id} room={room} />
-            )) : (
-              <p style={{ textAlign: "center", color: "GrayText" }}>You don't have any room listed yet</p>
-            )}
-
-            {Math.floor((numberOfRooms % 2)) !== 0 && (
-              <div style={{ height: "12.5rem", width: "32rem", padding: "0.2rem" }}></div>
-            )}
-
+    <>
+      <div className={styles.roomlist__container}>
+        {owner.ownerDetails.length < 1 ? <CreateOwner />
+        : (
+          <>
+          <CreateRoom />
+          <div className={styles.roomlist__lists}>
+            <div>
+              <div>
+                {data ? data?.map((room) => (
+                  <RoomCard key={room._id} room={room} />
+                )) : (
+                  <p style={{ textAlign: "center", color: "GrayText" }}>You don't have any room listed yet</p>
+                )}
+    
+                {Math.floor((numberOfRooms % 2)) !== 0 && (
+                  <div style={{ height: "12.5rem", width: "32rem", padding: "0.2rem" }}></div>
+                )}
+    
+              </div>
+            </div>
           </div>
-        </div>
+          </>
+        )}      
       </div>
-    </div>
+    </>
+
   )
 }
 
