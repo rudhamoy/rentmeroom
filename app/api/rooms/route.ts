5ee375128702e7b200b9dfbde8e3f20ec029a657
Rudhamoy Debbarma <rudhamoydebbarma@gmail.com>
2023-06-14 20:51:56 +0530

    room filter with tenants, roomcategory -will add more

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -4,6 +4,8 @@ import { createRoom } from "../../../services/controllers/roomControllers"
 import { roomModel } from "../../../services/models/roomModel"
 import { addressModel } from "../../../services/models/addressModel"
 
+import APIFeatures from "../../../services/utils/apiFeatures"
+
 // create room
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser()
@@ -26,18 +28,28 @@ export async function GET(request: Request) {
   const limit = parseInt(limitParams) || 5;
 
   let roomCategory = searchParams.get('roomCategory') || "All"
-  const roomCategoryOptions =["1R", "1RK", "1BHK", "2R", "2RK", "2BHK", "3BHK"];
-  
-  let tenants = searchParams.get("tenants") || "All"
-  const tenantsOptions =["All", "Students", "Family", "Girls", "Boys", "Bachelor"]
-
-  // if select all - render all room type else render only selected room type from query
-  // if more than one query  in room type, add it to the array  with comma separated in array via split method
+  const roomCategoryOptions = ["1R", "1RK", "1BHK", "2R", "2RK", "2BHK", "3BHK"];
   roomCategory === "All" ? (roomCategory = [...roomCategoryOptions]) : (roomCategory = searchParams.get('roomCategory').split(','));
 
-  tenants === "All" ? (tenants = [...tenantsOptions]) : (tenants = searchParams.get('tenants').split(','));
+  let tenants = searchParams.get('tenants') || "All"
+  const tenantOption = ["Students", "Family", "Girls", "Boys", "Bachelor", "All"]
+  tenants === "All" ? (tenants = [...tenantOption]) : (tenants = searchParams.get('tenants').split(','));
 
-  const allRooms = await roomModel.find().where('roomCategory').in([...roomCategory]).limit(limit).exec();
+  let allQuery = {
+    roomCategory,
+    tenants,
+  }
+
+  const apiFeatures = new APIFeatures(roomModel.find(), allQuery)
+  .filter()
+  .limit(limit)
+  .priceRange([searchParams.get('min') || 2000, searchParams.get('max') || 30000])
+
+  let allRooms = await apiFeatures.query;
+
+  const location = searchParams.get('location') || "";
+  const address = await addressModel.find({address: {$regex: location, $options: "i"}, pincode: searchParams.get('pincode')})
+  console.log('address', address)
 
   /**
    * get all rooms via room model and populate address data
@@ -60,7 +72,7 @@ export async function GET(request: Request) {
   return NextResponse.json({
     total,
     limit,
-    rooms: populatedAddress
+    rooms: populatedAddress,
   });
 
 }
---
+++ services/utils/apiFeatures.ts
@@ -0,0 +1,34 @@
+class APIFeatures {
+    constructor(query: any, queryStr: any) {
+        this.query = query;
+        this.queryStr = queryStr;
+    }
+
+
+    filter() {
+        const queryCopy = this.queryStr
+        this.query.find(queryCopy)
+        return this
+    }
+
+    priceRange(price: any) {
+        this.query.find({
+            pricePerMonth: {
+                $gte: price[0],
+                $lte: price[1]
+            }
+        })
+        return this
+    }
+
+
+
+    limit(limitNum: number) {
+        this.query = this.query.limit(limitNum)
+        return this
+    }
+
+
+}
+
+export default APIFeatures
